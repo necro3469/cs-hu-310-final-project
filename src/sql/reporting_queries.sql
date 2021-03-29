6b2c8ba04058dffcae43d0f34ea1d22e8d6a8577
necro3469 <80720857+necro3469@users.noreply.github.com>
2021-03-28 20:45:44 -0600

    Update reporting_queries.sql

--- src/sql/reporting_queries.sql
+++ src/sql/reporting_queries.sql
@@ -1,12 +1,6 @@
 /* Put your final project reporting queries here */
 USE cs_hu_310_final_project;
 
--- Example (remove before submitting)
--- Get all students
-SELECT
-    *
-FROM students;
-
 SELECT 
 	 students.first_name, 
     students.last_name,
